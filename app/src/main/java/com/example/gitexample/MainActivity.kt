a13a0e12e8447d243f65db8b9880ae9e3adbdcb2
Cristhian Rodríguez <crisrodam1996@gmail.com>
2022-10-01 07:56:16 -0500

    Delete prints

--- app/src/main/java/com/example/gitexample/MainActivity.kt
+++ app/src/main/java/com/example/gitexample/MainActivity.kt
@@ -7,16 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
-        print("Este es una linea de ejemplo")
     }
 }
